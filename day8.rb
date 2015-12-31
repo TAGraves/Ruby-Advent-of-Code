8adb107ef634a499310111d14e2dc05f4f18bd34
Tommy <tommy@infinitry.net>
2015-12-30 21:35:57 -0600

    Day 8
    Take a string with lots of escaping, and find the difference between
    the original string and the escaped/post-processed string.

---
+++ day8.rb
@@ -0,0 +1,48 @@
+INPUT = File.read("./day8input.txt").gsub(" ", "").split("\n")
+
+def parse_code(code)
+  return code.length
+end
+
+def parse_string(code)
+  return code.gsub(/\\x[a-fA-F\d]{2}/, "X").gsub(/\\"/, "X").gsub(/\\\\/, "X")[1...-1].length
+end
+
+def total_code(input)
+  total = 0
+  input.each do |code|
+    total += parse_code(code)
+  end
+  return total
+end
+
+def total_string(input)
+  total = 0
+  input.each do |code|
+    total += parse_string(code)
+  end
+  return total
+end
+
+def calc_difference(input)
+  return total_code(input) - total_string(input)
+end
+
+puts calc_difference(INPUT)
+
+=begin
+TEST1 = '""'
+TEST2 = '"abc"'
+TEST3 = '"aaa\"aaa"'
+TEST4 = '"\x27"'
+
+puts parse_code(TEST1)
+puts parse_string(TEST1)
+puts parse_code(TEST2)
+puts parse_string(TEST2)
+puts parse_code(TEST3)
+puts parse_string(TEST3)
+puts parse_code(TEST4)
+puts parse_string(TEST4)
+puts calc_difference([TEST1, TEST2, TEST3, TEST4])
+=end
