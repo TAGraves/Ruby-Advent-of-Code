8c86319b5e4f7b6fa09d83901092d09d4b36cc2c
Tommy <tommy@infinitry.net>
2015-12-30 22:50:01 -0600

    Day 8 Part 2
    Now encode the strings instead of decoding them.

--- day8.rb
+++ day8.rb
@@ -8,6 +8,11 @@ def parse_string(code)
   return code.gsub(/\\x[a-fA-F\d]{2}/, "X").gsub(/\\"/, "X").gsub(/\\\\/, "X")[1...-1].length
 end
 
+def encode_string(code)
+  return ('"' + code.gsub(/\\/, "\\\\\\").gsub(/"/, '\\"') + '"').length
+end
+
+
 def total_code(input)
   total = 0
   input.each do |code|
@@ -24,18 +29,34 @@ def total_string(input)
   return total
 end
 
+def total_encode(input)
+  total = 0
+  input.each do |code|
+    total += encode_string(code)
+  end
+  return total
+end
+
 def calc_difference(input)
   return total_code(input) - total_string(input)
 end
 
-puts calc_difference(INPUT)
+def calc_encoded_difference(input)
+  return total_encode(input) - total_code(input)
+end
 
-=begin
+
+
+
+#puts calc_difference(INPUT)
+puts calc_encoded_difference(INPUT)
+
 TEST1 = '""'
 TEST2 = '"abc"'
 TEST3 = '"aaa\"aaa"'
 TEST4 = '"\x27"'
 
+=begin
 puts parse_code(TEST1)
 puts parse_string(TEST1)
 puts parse_code(TEST2)
@@ -46,3 +67,5 @@ puts parse_code(TEST4)
 puts parse_string(TEST4)
 puts calc_difference([TEST1, TEST2, TEST3, TEST4])
 =end
+
+puts calc_encoded_difference([TEST1, TEST2, TEST3, TEST4])
