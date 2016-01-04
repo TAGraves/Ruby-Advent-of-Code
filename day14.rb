b843d880951c1c05cbddca49304adfe5e4c02930
Tommy <tommy@infinitry.net>
2016-01-04 01:26:27 -0500

    Day 14 Part 2
    Award points to each reindeer in the lead at any given second.

--- day14.rb
+++ day14.rb
@@ -1,5 +1,5 @@
 def get_reindeer_info(input)
-  reindeer_array = []
+  reindeer_array = {}
   input.each do |info|
     data = info.match(/(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds/)
     reindeer = {}
@@ -7,38 +7,57 @@ def get_reindeer_info(input)
     reindeer['speed'] = data[2].to_i
     reindeer['time'] = data[3].to_i
     reindeer['rest'] = data[4].to_i
-    reindeer_array.push(reindeer)
+    reindeer['points'] = 0
+    reindeer_array[data[1]] = (reindeer)
   end
   return reindeer_array
 end
 
-def calc_reindeer_distance(input, time)
-  reindeer_array = get_reindeer_info(input)
+def calc_reindeer_distance(reindeer_array, time)
   longest = 0
   winner = {}
-  reindeer_array.each do |reindeer|
+  winnerArr = []
+  reindeer_array.each do |key, reindeer|
     isResting = false
     distance = 0
-    while time > 0
+    timeClone = time
+    while timeClone > 0
       if isResting
-        time += -reindeer['rest']
+        timeClone += -reindeer['rest']
         isResting = false
-      elsif time < reindeer['time']
-        distance += reindeer['speed'] * time
-        time = 0
+      elsif timeClone < reindeer['time']
+        distance += reindeer['speed'] * timeClone
+        timeClone = 0
       else
         distance += reindeer['speed'] * reindeer['time']
-        time += -reindeer['time']
+        timeClone += -reindeer['time']
         isResting = true
       end
     end
     if distance > longest
       longest = distance
       winner = reindeer
+      winnerArr = [winner]
+    elsif distance == longest
+      winnerArr.push(reindeer)
     end
   end
-  winner['distance'] = longest
-  return winner
+  winnerArr.each do |win|
+    win['distance'] = longest
+  end
+  return winnerArr
+end
+
+def calc_reindeer_points(reindeer_array, time)
+  time.times do |i|
+    winning = calc_reindeer_distance(reindeer_array, i+1)
+    winning.each do |winner|
+      reindeer_array[winner['name']]['points'] += 1
+    end
+  end
+  
+  reindeer_arr = reindeer_array.values.sort! { |x,y| y['points'] <=> x['points']}
+  return reindeer_arr
 end
 
 TEST1 = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
@@ -54,4 +73,5 @@ Dasher can fly 14 km/s for 3 seconds, but then must rest for 38 seconds.
 Dancer can fly 3 km/s for 16 seconds, but then must rest for 37 seconds.
 Prancer can fly 25 km/s for 6 seconds, but then must rest for 143 seconds.".split("\n")
 
-puts calc_reindeer_distance(INPUT, 2503)
+#puts calc_reindeer_points(get_reindeer_info(TEST1), 1000)[0]['points']
+puts calc_reindeer_points(get_reindeer_info(INPUT), 2503)[0]['points']
